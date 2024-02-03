678bf76b6c8c7fef43998f4d08a274b2270b12ce
Miikka K <miikkak@tuta.io>
2024-02-02 20:06:17 -0600

    reverse string

---
+++ reverse-string.js
@@ -0,0 +1,8 @@
+// given a string, return that string reversed... easy af
+
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Hello World and Coders"))
