90a334c2712a088b4970b3e1615a5f9aded13d26
Miikka K <miikkak@tuta.io>
2024-02-02 20:11:16 -0600

    find longest word in a string

---
+++ find-longest-word.js
@@ -0,0 +1,10 @@
+//find longest word in a string.
+//
+
+
+function findLongestWord(str) {
+    return str.split(" ").sort((a, b) => b.length - a.length)[0]
+}
+
+
+console.log(findLongestWord("hi this is a difficult coding excercise. not performances"))
