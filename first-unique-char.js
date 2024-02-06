e74f0a826a56a24312d8df29bc5f741301fdeb10
Miikka K <miikkak@tuta.io>
2024-02-06 11:11:31 -0600

    first uniq char

---
+++ first-unique-char.js
@@ -0,0 +1,15 @@
+var firstUniqChar = function(s) {
+  let map = {};
+  for (char of s) {
+    if (!map[char]) {
+      map[char] = 0
+    }
+    map[char] += 1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if(map[s[i]] === 1) return i;
+  }
+  return -1;
+};
+
+console.log(firstUniqChar("aabfaeaf"))
