cd123bd73fc99649f4f74896ec89fc08dfbe6b6b
Miikka K <miikkak@tuta.io>
2024-02-06 11:36:54 -0600

    isPalindrome leetcode

---
+++ number-palindrome.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let toArray = new String(x).split("")
+    let j = toArray.length - 1;
+    for(let i = 0; i < toArray.length; i++) {
+      if(toArray[i] !== toArray[j]) {
+        return false;
+      }
+      j--;
+    }
+  return true;
+};
