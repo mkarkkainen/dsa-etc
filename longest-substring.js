79243a56266bf698a6641380d21a4632e0f14dcc
Miikka K <miikkak@tuta.io>
2024-02-09 11:14:13 -0600

    longest substr - leetcode

---
+++ longest-substring.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const map = new Map();
+    let start = 0;
+    let result = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        if(map.has(s[i])) {
+            start = Math.max(map.get(s[i]) + 1, start)
+        }
+        map.set(s[i], i);
+        result = Math.max(i - start +1, result);
+    }
+    return result;
+};
