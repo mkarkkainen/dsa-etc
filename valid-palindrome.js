29813577e47d45a502fbfbf58d8b324cc7758674
Miikka K <miikkak@tuta.io>
2024-02-14 23:28:57 -0600

    valid palindrome - leetcode

---
+++ valid-palindrome.js
@@ -0,0 +1,22 @@
+//Leetcode 125. Valid Palindrome
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome(s) {
+  s = s.replace(/[\W_]/g, "").toLowerCase();
+  
+  let left = 0;
+  let right = s.length - 1;
+  
+  while(right >= left) {
+    if(s[right] !== s[left]) {
+      return false;
+    }
+    right--;
+    left++;
+  }
+  return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
