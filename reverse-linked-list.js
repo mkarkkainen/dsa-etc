64abcee5cbf09e5c96b50201325c05e524a0cf02
Miikka K <miikkak@tuta.io>
2024-02-15 14:48:18 -0600

    reverse linked list, iterative and recursive - leetcode

---
+++ reverse-linked-list.js
@@ -0,0 +1,46 @@
+//Leetcode 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// iterative method
+function reverseList(head) {
+    let prev = null
+    while(head) {
+        [head.next, prev, head] = [prev, head, head.next]
+    }
+    return prev
+}
+
+//iterative without destructuring
+function itReverseList(head) {
+    let prev = null
+    let curr = head
+    while(curr) {
+        let tmp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = tmp
+    }
+    return prev
+}
+
+// recursive method
+function recurseReverseList(head) {
+    if(!head || !head.next) return head
+
+    let tmp = recurseReverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return tmp
+}
+
+
