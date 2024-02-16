3ec1f76744570a17432ab6357d0191247a5fcbfd
Miikka K <miikkak@tuta.io>
2024-02-15 23:44:47 -0600

    first palindrome - leetcode

---
+++ first-palindrome.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+
+function isPalindrome(s) {
+  s = s.replace(/[\W_]/g, "").toLowerCase();
+  
+  let left = 0;
+  let right = s.length - 1;
+  
+  while(right >= left) {
+    if(s[right] !== s[left]) {
+      return false;
+    }
+    right--;
+    left++;
+  }
+  return true;
+};
+var firstPalindrome = function(words) {
+    for(const word of words) {
+        if(isPalindrome(word)) {
+            return word
+        }
+    }
+    return ""
+};
