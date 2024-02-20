aa34f39e31f504ff4462970e43c8007050deee5f
Miikka K <miikkak@tuta.io>
2024-02-20 12:11:10 -0600

    balanced binary tree - leetcode

---
+++ balanced-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+function checkHeight(node) {
+    if(!node) return 0
+
+    const l = checkHeight(node.left)
+    const r = checkHeight(node.right)
+
+    if(l===false || r===false || Math.abs(l - r) > 1) {
+        return false
+    }
+
+    return Math.max(l, r) + 1;
+}
+var isBalanced = function(root) {
+    if(!root) return true
+    return checkHeight(root) !== false
+};
