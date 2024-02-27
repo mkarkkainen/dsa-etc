d147e382dfd216c6c025cd19bf68f80739374400
Miikka K <miikkak@tuta.io>
2024-02-27 17:52:04 -0600

    climbing stairs

---
+++ 70-climbing-stairs.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/climbing-stairs
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let a = 0;
+    let b = 1;
+    let c = 0;
+
+    for (let i = 0; i < n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+};
