83288d015944c5e3607963d9aac67f70d704527f
Miikka K <miikkak@tuta.io>
2024-02-28 11:43:07 -0600

    majority element

---
+++ 169-majority-element.js
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/majority-element/
+//
+// Boyer-Moore Voting Algorithm
+// https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let res = 0;
+    let major = 0;
+
+    for (let num of nums) {
+        if(major === 0) {
+            res = num;
+        };
+
+        major += num === res ? 1 : -1;
+    };
+
+    return res;
+};
