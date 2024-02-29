efa29f3e649690941a40b160d8afbcacc7d88aa9
Miikka K <miikkak@tuta.io>
2024-02-29 09:05:33 -0600

    description to majority element

--- 169-majority-element.js
+++ 169-majority-element.js
@@ -2,10 +2,15 @@
 //
 // Boyer-Moore Voting Algorithm
 // https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+//
+// +1 to maj if same num, else -1, if maj gets to zero change res
+
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
+
 var majorityElement = function(nums) {
     let res = 0;
     let major = 0;
